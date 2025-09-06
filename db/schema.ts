c8f248921c23580a57c068604272e9e9f152e8bb
Ayush Singh <ayushpno@gmail.com>
2025-09-06 22:17:09 +0530

    Codebase pruning

--- db/schema.ts
+++ db/schema.ts
@@ -5,55 +5,14 @@ import {
   verification as verificationTable
 } from "./schema/auth-schema"
 
-import {
-  chapters as chaptersTable,
-  chapterContent as chaptersContentTable
-} from "./schema/form"
-
-import {
-  chapters as exampleChaptersTable,
-  chapterContent as exampleChaptersContentTable
-} from "./schema/example-editor"
-
-import {
-  pageData as pageDataTable,
-  tags as TagsTable,
-  pageTags as pageTagsTable,
-  pageStatusEnum as statusEnum
-} from "./schema/editor-schema"
-
-import {
-  content as contentTable
-} from "./schema/content-schema"
-
 export const user = userTable;
 export const session = sessionTable;
 export const account = accountTable;
 export const verification = verificationTable;
-export const pageData = pageDataTable;
-export const tags = TagsTable;
-export const pageTags = pageTagsTable;
-export const pageStatusEnum = statusEnum;
-export const content = contentTable;
-
-export const chapters = chaptersTable;
-export const chapterContent = chaptersContentTable;
-
-export const chaptersExample = exampleChaptersTable;
-export const chapterContentExample = exampleChaptersContentTable;
 
 export const schema = {
   user,
   session,
   account,
   verification,
-  tags,
-  pageData,
-  pageTags,
-  statusEnum,
-  content,
-  chapters,
-  chapterContent,
-  chaptersExample,
-  chapterContentExample,
 }
